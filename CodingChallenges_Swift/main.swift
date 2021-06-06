c13f339a85e84699ea5e48446af86fd8d11e3969
narendra.vadde <narendra.vadde@Pavans-Mac-mini.local>
2021-06-06 22:43:58 +0530

    unique letters

--- CodingChallenges_Swift/main.swift
+++ CodingChallenges_Swift/main.swift
@@ -7,5 +7,15 @@
 
 import Foundation
 
-print("Hello, World!")
+//MARK:- Uniue letters
+
+//Sample input and output
+//    •    The string “No duplicates” should return true.  
+//    •    The string “abcdefghijklmnopqrstuvwxyz” should return true.  
+//    •    The string “AaBbCc” should return true because the challenge is case-sensitive.  
+//    •    The string “Hello, world” should return false because of the double Ls and double Os.
+
+let result1a = challenge1a(string: "Hello, world")
+let results1b = challenge1b(string: "abcdefghijklmnopqrstuvwxyz")
+print(results1b)
 
